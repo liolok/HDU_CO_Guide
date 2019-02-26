456793c16f5fdbd5fc135b109f030cdea12460a3
liolok <liolokit@gmail.com>
2019-02-26 15:49:39 +0800

    refactor board level code in test project

--- 01_Test/Board.v
+++ 01_Test/Board.v
@@ -1,13 +1,11 @@
 `timescale 1ns / 1ps
 // 通过数据输入输出测试开关、LED、数码管；通过数码管使能测试按钮。
-module Board(sw, led, swb,
-    clk, which, seg, enable); // 数码管
-    parameter X = 32; // 数据位宽
-    input [X:1] sw; // 开关输入数据
-    output [X:1] led;
-    assign led[X:1] = sw[X:1]; // 直接输出到 LED
+module Board(sw, led, swb, clk, which, seg, enable);
+    input [1:32] sw; // 开关输入数据
+    output [1:32] led;
+    assign led = sw; // 直接输出到 LED
 
-    input clk;
+    input clk; // 数码管相关
     output [2:0] which;
     output [7:0] seg;
     output reg enable = 1; // 默认开启数码管使能
@@ -17,9 +15,8 @@ module Board(sw, led, swb,
         .which(which),  // 片选编码
         .seg(seg));     // 段选信号
 
-    parameter N = 6; //按钮个数
-    input [N:1] swb;
-    assign toggle = |swb[N:1]; // 按下任意按钮切换数码管使能
+    input [1:6] swb;
+    assign toggle = |swb; // 按下任意按钮切换数码管使能
     always @(posedge toggle) enable <= ~enable;
 
-endmodule
+endmodule // Board
